c6306e50671c03bb2a1c78f11604d16f2d6ab3d9
suriyaa26 <147575543+suriyaa26@users.noreply.github.com>
2024-07-12 20:22:10 -0400

    Add files via upload

---
+++ Sristi_HW1_1.cpp
@@ -0,0 +1,17 @@
+/*#include <iostream>
+using namespace std;
+
+int main () {
+  string name;
+  string number;
+  cout << "Hello.";
+  cout << "What is your name?" << endl;
+  cin >> name;
+  cout << "Hello " << name << ". What is your favorite number?" << endl;
+  cin >> number; 
+  cout << "Name : " << name << endl;
+  cout << "Favorite number: " << number << endl;
+  return 0;
+  
+}
+*/
